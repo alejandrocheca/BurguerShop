adfc0c806d227cd59d40096adf86a0dc2f79c685
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-07-04 18:04:23 +0200

    Create CreateOrUpdateHamburguerDTO.java
    Create file CreateOrUpdateHamburguerDTO in hamburguerApplication

---
+++ demo/src/main/java/com/example/demo/application/hamburguerApplication/CreateOrUpdateHamburguerDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.application.hamburguerApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateOrUpdateHamburguerDTO {
+    @NotBlank
+    private String name;
+    @NonNull
+    private String image;
+    @NotEmpty
+    private Set<UUID> ingredients = new HashSet<UUID>();   
+}
