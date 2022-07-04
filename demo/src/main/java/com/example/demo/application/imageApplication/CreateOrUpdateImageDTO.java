d390bcce347b2692da3113da7052a9fce8263616
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-07-04 18:09:34 +0200

    Create CreateOrUpdateImageDTO.java
    Fix errors in CreateOrUpdateImageDTO

---
+++ demo/src/main/java/com/example/demo/application/imageApplication/CreateOrUpdateImageDTO.java
@@ -0,0 +1,16 @@
+package com.example.demo.application.imageApplication;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateOrUpdateImageDTO {
+    @NotEmpty
+    public byte[] content;
+}
