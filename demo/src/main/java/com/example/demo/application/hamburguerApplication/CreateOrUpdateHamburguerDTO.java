d888c2841f3929ca6a5bb4223d7db2f2ec5bcc38
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-07-04 18:16:46 +0200

    Update CreateOrUpdateHamburguerDTO.java

--- demo/src/main/java/com/example/demo/application/hamburguerApplication/CreateOrUpdateHamburguerDTO.java
+++ demo/src/main/java/com/example/demo/application/hamburguerApplication/CreateOrUpdateHamburguerDTO.java
@@ -1,10 +1,10 @@
 package com.example.demo.application.hamburguerApplication;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -15,7 +15,6 @@ public class CreateOrUpdateHamburguerDTO {
     @NotBlank
     private String name;
     @NonNull
-    //private UUID image;
     private String image;
     @NotEmpty
     private Set<UUID> ingredients = new HashSet<UUID>();        
