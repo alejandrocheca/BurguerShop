8c0a424d19c835053c67e9f71ad8eaf797f66071
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-07-13 16:44:37 +0200

    Create IngredientProjection.java

---
+++ demo/src/main/java/com/example/demo/domain/ingredientDomain/IngredientProjection.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.ingredientDomain;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public interface IngredientProjection {
+    public UUID getId();
+    public String getName();
+    public BigDecimal getPrice(); 
+}
