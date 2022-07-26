70401092ea82bba4679e947c4f3c3f32e3af99cd
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-07-26 10:57:53 +0200

    Create app-routing.module.ts

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HamburguerListComponent } from './hamburguer/components/hamburguer.component';
+import { UserEditComponent } from './user/components/user.component';
+
+const routes: Routes = [
+  {path: '', component: HamburguerListComponent, pathMatch: 'full'},
+  { path: 'users',loadChildren: () => import('./security/security.module').then(mod => mod.SecurityModule)},
+  { path: 'orders',loadChildren: () => import('./orders/order.module').then(mod => mod.OrdersModule)},
+  { path: 'hamburguers', loadChildren: () => import('./hamburguer/hamburguer.module').then(mod => mod.HamburguerModule)},
+  { path: 'ingredients', loadChildren: () => import('./ingredient/ingredient.module').then(mod => mod.IngredientModule)},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
