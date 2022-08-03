f0c8869e06afe15b3f6f3ec2a96d9d1de3df20bf
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-08-03 15:18:35 +0200

    Create user.component.spec.ts

---
+++ frontend/src/app/comment/user.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PizzaComponent } from './user.component';
+
+describe('PizzaComponent', () => {
+  let component: PizzaComponent;
+  let fixture: ComponentFixture<PizzaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PizzaComponent ]
+    })
+    .compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
