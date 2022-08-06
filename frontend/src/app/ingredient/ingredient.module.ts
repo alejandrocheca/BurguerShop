d860b68216073b9b899678c441a5d93bffb2691e
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-08-06 10:01:58 +0200

    Create ingredient.module.ts

---
+++ frontend/src/app/ingredient/ingredient.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IngredientComponent, IngredientEditComponent, IngredientListComponent, IngredientRecipeComponent, IngredientsAddComponent, IngredientViewComponent, INGREDIENT_COMPONENTS } from './components/ingredient.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {TableModule} from 'primeng/table';
+
+const routes: Routes = [
+  { path: '', component: IngredientListComponent},
+  { path: 'add', component: IngredientsAddComponent},
+  { path: ':id/edit', component: IngredientEditComponent},
+  { path: ':id', component: IngredientViewComponent},
+];
+
+@NgModule({
+  declarations: [INGREDIENT_COMPONENTS, IngredientComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    TableModule, CommonModule
+  ],
+  exports:[INGREDIENT_COMPONENTS, IngredientRecipeComponent]
+})
+export class IngredientModule { }
