173cda58c9ae6ae1ec3869d6b33a6882d10846e9
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-08-09 09:57:04 +0200

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HamburguerListComponent } from './hamburger/components/hamburguer.component';
+import { HamburguerListComponent } from './hamburguer/components/hamburguer.component';
 import { UserEditComponent } from './user/components/user.component';
 
 const routes: Routes = [
   {path: '', component: HamburguerListComponent, pathMatch: 'full'},
   { path: 'users',loadChildren: () => import('./security/security.module').then(mod => mod.SecurityModule)},
   { path: 'orders',loadChildren: () => import('./orders/order.module').then(mod => mod.OrdersModule)},
-  { path: 'hamburguers', loadChildren: () => import('./hamburger/hamburguer.module').then(mod => mod.HamburguerModule)},
+  { path: 'hamburguers', loadChildren: () => import('./hamburguer/hamburguer.module').then(mod => mod.HamburguerModule)},
   { path: 'ingredients', loadChildren: () => import('./ingredient/ingredient.module').then(mod => mod.IngredientModule)},
 
 ];
