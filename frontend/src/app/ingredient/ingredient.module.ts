3f55405ceeab5bc149de5ee780fa2eced694410c
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2022-10-10 11:29:49 +0200

    Update ingredient.module.ts

--- frontend/src/app/ingredient/ingredient.module.ts
+++ frontend/src/app/ingredient/ingredient.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngredientComponent, IngredientEditComponent, IngredientListComponent, IngredientRecipeComponent, IngredientsAddComponent, IngredientViewComponent, INGREDIENT_COMPONENTS } from './components/ingredient.component';
